9103a06611b7343a1975fcc359902a9bd176c262
MarcioBittencourt <marciobitt97@gmail.com>
2021-03-04 15:38:28 -0300

    Implementação do desafio database upload.

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,37 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column('varchar')
   category_id: string;
 
+  @Column('timestamp')
   created_at: Date;
 
+  @Column('timestamp')
   updated_at: Date;
 }
 
